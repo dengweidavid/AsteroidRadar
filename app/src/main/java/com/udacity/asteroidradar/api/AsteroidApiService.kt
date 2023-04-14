b1eb0f1a5d706c2c1d3b96672efbfa8a5f12fc74
dengweidavid <wei.deng@machineswithvision.com>
2023-04-14 14:33:28 +0100

    Add picture of day

---
+++ app/src/main/java/com/udacity/asteroidradar/api/AsteroidApiService.kt
@@ -0,0 +1,31 @@
+package com.udacity.asteroidradar.api
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.udacity.asteroidradar.Constants.BASE_URL
+import com.udacity.asteroidradar.PictureOfDay
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface AsteroidService {
+    @GET("neo/rest/v1/feed")
+    suspend fun getAsteroids(@Query("api_key") api_key: String): String
+
+    @GET("planetary/apod")
+    suspend fun getPictureOfDay(@Query("api_key") api_key: String): PictureOfDay
+}
+
+object AsteroidApi {
+    val retrofitService : AsteroidService by lazy { retrofit.create(AsteroidService::class.java) }
+}
--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,6 +1,38 @@
 package com.udacity.asteroidradar.main
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.udacity.asteroidradar.Constants.API_KEY
+import com.udacity.asteroidradar.PictureOfDay
+import com.udacity.asteroidradar.api.AsteroidApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainViewModel : ViewModel() {
+
+    private val _pictureOfDay = MutableLiveData<PictureOfDay>()
+    val pictureOfDay: LiveData<PictureOfDay>
+        get() = _pictureOfDay
+
+    init {
+        viewModelScope.launch {
+            refreshPictureOfDay()
+        }
+    }
+
+    private suspend fun refreshPictureOfDay() {
+        withContext(Dispatchers.IO) {
+            try {
+                _pictureOfDay.postValue(
+                    AsteroidApi.retrofitService.getPictureOfDay(API_KEY)
+                )
+            } catch (err: Exception) {
+                Log.e("refreshPictureOfDay", err.printStackTrace().toString())
+            }
+        }
+    }
 }
